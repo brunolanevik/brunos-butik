93fd0691cc682bc884eb3de249119df064b4c7ac
Bruno Lanevik <bruno@iveo.se>
2022-11-25 12:56:19 +0100

    Lazyload

--- components/Product.vue
+++ components/Product.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-80 mb-4 block">
-    <img :alt="title" class="w-full h-80 mb-8 rounded" :src="image">
+    <img :alt="title" class="w-full h-80 mb-8 rounded" :src="image" loading="lazy">
     <h3 class="w-full mb-4 text-2xl">{{title}}</h3>
     <content class="w-full">{{description}}</content>
     <AddToCart :product="product"></AddToCart>
